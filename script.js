8a32f61c3eae50fbceb8ca34ed5c59a87b10ca11
Belhassen <152809495+BelhassenB@users.noreply.github.com>
2025-11-23 12:34:57 +0100

    Create a function to play a round

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let computerScore = 0
 function getComputerChoice() {
     const computerOutput = Math.floor(Math.random() * 3)
     console.log(computerOutput)
-    const rock = "Rock"
+    const rock = "rock"
     const paper = "paper"
-    const scissor = "Scissor"
+    const scissor = "scissor"
     if (computerOutput === 0) {
         return rock
     } else if (computerOutput === 1) {
@@ -24,12 +24,21 @@ function getHumanChoice() {
 const humanSelection = getHumanChoice()
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "rock" && computerChoice === "Scissor") {
-        const result = `Computer Choise is: ${computerChoice}, You won!`
-        console.log(result)
+    if (
+        (humanChoice === "rock" && computerChoice === "scissor") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissor" && computerChoice === "paper")        
+    
+    ) { console.log(`Camputer choice is: ${computerChoice}. You won this round!`)
         humanScore++
-        
-    } 
+              
+    } else if (humanChoice === computerChoice) console.log("it's a draw!")
+      else 
+        console.log(`Computer choice is ${computerChoice}. You lost this round:`)
+        computerScore++
+      
         
 }
 playRound(humanSelection, computerSelection)
+console.log(humanScore, computerScore)
+
