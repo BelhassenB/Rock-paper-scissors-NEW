eb3835268205d31664f386e8c051818da28e6f5c
Belhassen <152809495+BelhassenB@users.noreply.github.com>
2025-11-23 14:04:16 +0100

    Add a function to play five round and declare winner

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let humanScore = 0
 let computerScore = 0
 function getComputerChoice() {
     const computerOutput = Math.floor(Math.random() * 3)
-    console.log(computerOutput)
     const rock = "rock"
     const paper = "paper"
     const scissor = "scissor"
@@ -13,15 +12,14 @@ function getComputerChoice() {
     } 
     return scissor
 }
-const computerSelection = getComputerChoice()
-console.log(computerSelection)
+
+
 
 function getHumanChoice() {
     return (prompt("Choose your weapon: Rock, Paper or Scissor")).toLowerCase()
     
     
 }
-const humanSelection = getHumanChoice()
 
 function playRound(humanChoice, computerChoice) {
     if (
@@ -32,13 +30,28 @@ function playRound(humanChoice, computerChoice) {
     ) { console.log(`Camputer choice is: ${computerChoice}. You won this round!`)
         humanScore++
               
-    } else if (humanChoice === computerChoice) console.log("it's a draw!")
-      else 
-        console.log(`Computer choice is ${computerChoice}. You lost this round:`)
-        computerScore++
-      
-        
+    } else if (humanChoice === computerChoice) {
+        console.log(`Computer choice is ${computerChoice}. it's a draw`) 
+    } else {
+        console.log(`Computer choice is ${computerChoice}. You lost this round`) 
+        computerScore++  
+    }            
+              
 }
-playRound(humanSelection, computerSelection)
-console.log(humanScore, computerScore)
 
+
+
+function playGame() {
+    while ((humanScore + computerScore) < 5 ) {
+        const humanSelection = getHumanChoice()
+        const computerSelection = getComputerChoice()
+        
+        playRound(humanSelection, computerSelection)
+        console.log(`You: ${humanScore}`, `Computer: ${computerScore}`)
+        console.log(humanScore, computerScore)
+    }
+    (humanScore > computerScore) ?
+    console.log(`You won against the computer: ${humanScore} VS ${computerScore}`):
+    console.log(`You lost against the computer: ${humanScore} VS ${computerScore}`)
+}
+playGame()
